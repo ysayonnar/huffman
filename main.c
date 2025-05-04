425803753d80cd13cc137418bbc477065fb6bcaf
ysayonnar <ysayonnar@gmail.com>
2025-05-04 22:13:47 +0300

    just test

--- main.c
+++ main.c
@@ -1,34 +1,35 @@
+#include "src/hashtable/hashtable.h"
 #include "stdio.h"
 #include "stdlib.h"
-#include "src/hashtable/hashtable.h"
 
-int main(){
-	FILE *file = fopen("text.txt", "r");
+// just test
+int main() {
+  FILE *file = fopen("text.txt", "r");
 
-	if(file == NULL){
-		perror("Error while file opening");
-		return 1;
-	}
+  if (file == NULL) {
+    perror("Error while file opening");
+    return 1;
+  }
 
-	HashTable ht;
-	initTable(&ht);
+  HashTable ht;
+  initTable(&ht);
 
-	int ch;
-	while((ch = fgetc(file)) != EOF){
-		int freq = 0;
-		getTable(&ht, ch, &freq);
-		insertTable(&ht, ch, freq+1);
-	}
+  int ch;
+  while ((ch = fgetc(file)) != EOF) {
+    int freq = 0;
+    getTable(&ht, ch, &freq);
+    insertTable(&ht, ch, freq + 1);
+  }
 
-	for (int i = 0; i < 256; i++) {
-		int freq;
-		int isFound = getTable(&ht, i, &freq);
-		if (!isFound){
-			continue;
-		}
+  for (int i = 0; i < 256; i++) {
+    int freq;
+    int isFound = getTable(&ht, i, &freq);
+    if (!isFound) {
+      continue;
+    }
 
-		printf("%c: %d\n", i, freq);
-	}
+    printf("%c: %d\n", i, freq);
+  }
 
-	fclose(file);
+  fclose(file);
 }
